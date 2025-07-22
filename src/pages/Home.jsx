dadd94b93aa576f592695a8423fb09d744fb6cf9
Prince Lakhwani <princelakhwani1608@gmail.com>
2025-07-22 11:33:41 +0530

    Prince

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '/src/Styles/Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to my Portfolio</h1>
+    </div>
+  );
+}
+
+export default Home;
