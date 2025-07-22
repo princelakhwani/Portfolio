ad2608093cee5a7e22c21a879a1ec83955d3f3c7
Prince Lakhwani <princelakhwani1608@gmail.com>
2025-07-22 14:03:00 +0530

    Prince

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import NavbarHome from '/src/component/NavbarHome.jsx';
-import '/src/Styles/Home.css';
+import '/src/Styles/Home/Home.css';
 
 function Home() {
   return (
     <div className="home">
       <NavbarHome />
-      <div className="home-content">
-        <h1>Welcome to my Portfolio</h1>
-      </div>
     </div>
   );
 }
